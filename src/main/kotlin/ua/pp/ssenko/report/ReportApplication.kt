dbdce5c25a627ff2a3cd6c1e62a246d539363aca
ssenko <sergeysenja1992@gmail.com>
2018-10-05 08:14:21 +0300

    add code

---
+++ src/main/kotlin/ua/pp/ssenko/report/ReportApplication.kt
@@ -0,0 +1,11 @@
+package ua.pp.ssenko.report
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReportApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReportApplication>(*args)
+}
---
+++ src/test/kotlin/ua/pp/ssenko/report/ReportApplicationTests.kt
@@ -0,0 +1,16 @@
+package ua.pp.ssenko.report
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ReportApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
